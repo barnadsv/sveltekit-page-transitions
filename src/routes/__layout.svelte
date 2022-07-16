1ec9946823f301a0d0d3d2b3fa6324fb5616d1bd
barnadsv <barnadsv@gmail.com>
2022-07-16 15:33:28 -0300

    Dark mode e botao de toggle

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,6 +8,7 @@
 
 <script>
   import PageTransition from "$lib/components/PageTransition.svelte";
+  import Button from '$lib/components/Button.svelte'
   export let pathname;
 </script>
 
@@ -15,6 +16,9 @@
   <nav>
     <a href="/">Home</a>
     <a href="/about">About</a>
+    <Button>
+      Toggle
+    </Button>
   </nav>
 
   <!-- 2. Pass `pathname` prop to the component so it knows when to transition -->
@@ -30,4 +34,20 @@
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
       Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
+	:global(body) {
+		background-color: #f2eee2;
+		color: #0084f6;
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+  :global(body a) {
+		color: #0084f6;
+		transition: color 0.3s
+	}
+	:global(body.dark-mode a) {
+		color: #bfc2c7;
+	}
 </style>
